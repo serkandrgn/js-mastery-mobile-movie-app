c56c735edbc1cdc26b308f1592c438ef48197886
serkandrgn <durgun.serkan@gmail.com>
2025-03-12 16:38:31 +0300

    tmdb fetch started.

---
+++ services/useFetch.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error("An error has occurred"));
+    } finally {
+      setLoading(false);
+    }
+  };
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, [autoFetch]);
+  return { data, error, loading, reset, refetch: fetchData };
+};
+
+export default useFetch;
